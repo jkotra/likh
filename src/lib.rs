385e98dead8fcf64e5f7530fc3d029e355295f90
Jagadeesh Kotra <jagadeesh@stdin.top>
2021-01-14 19:03:43 +0530

    refactor!: use String instead of &str
    [+] Result oriented design

--- src/lib.rs
+++ src/lib.rs
@@ -2,38 +2,45 @@ use std::fs;
 use std::fs::OpenOptions;
 use std::io::prelude::*;
 
-/// Likh - library to read and write to text and CSV files.
+/// Likh - simple library to read and write to text and CSV files.
 
-
-pub fn read_from_file(filename: &str) -> String{
-    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
-    return contents;
+pub fn read_from_file(filename: &str) -> Result<String, std::io::Error> {
+    match fs::read_to_string(filename) {
+        Ok(c) => return Ok(String::from(c)),
+        Err(why) => return Err(why),
+    };
 }
 
-pub fn write_to_file(filename: &str, content: &str){
-
-    fs::write(filename, content).expect("Cannot open file!");
+pub fn write_to_file(filename: &str, content: &str) -> Result<bool, std::io::Error> {
+    match fs::write(filename, content) {
+        Ok(_) => return Ok(true),
+        Err(why) => return Err(why),
+    }
 }
 
-pub fn read_from_csv<'a> (content: &'a str) -> Vec<Vec<&'a str>> {
-
+pub fn read_from_csv(content: String) -> Vec<Vec<String>> {
     let mut data = vec![];
 
-    for line in content.split("\n"){
+
+    for line in content.as_str().split("\n") {
         let mut line_vec = vec![];
-        for row in line.split(","){
-            line_vec.push(row);
+        for row in line.split(",") {
+            line_vec.push(String::from(row));
         }
         data.push(line_vec);
     }
     return data;
 }
 
-pub fn write_to_csv<'a> (content: Vec<Vec<&str>>,file_path: &str) {
-    let mut file = OpenOptions::new().append(true).create(true).open(file_path).unwrap();
-    for row in content{
+pub fn write_to_csv(content: Vec<Vec<String>>, file_path: &str) {
+    let mut file = OpenOptions::new()
+        .append(true)
+        .create(true)
+        .open(file_path)
+        .unwrap();
+    for row in content {
         let row_content = row.join(",");
-        write!(&mut file, "{}\n",row_content).expect("Unable to write to file!")
+        write!(&mut file, "{}\n", row_content).expect("Unable to write to file!")
     }
 }
 
@@ -44,16 +51,15 @@ mod tests {
         assert_eq!(2 + 2, 4);
     }
     
-    #[test]
-    fn read_test() {
-        let output = super::read_from_file("tests/data.txt");
-        assert_eq!(output, "Hello, World!\n");
-    }
 
     #[test]
-    fn write_test() {
-        super::write_to_file("tests/writetome.txt", "Hello, World!");
-        let read_output = super::read_from_file("tests/writetome.txt");
-        assert_eq!(read_output, "Hello, World!");
+    fn read_write_csv_test() {
+        let content = vec![vec![String::from("one"), String::from("two"), String::from("three"), String::from("four")]];
+        super::write_to_csv(content, "target/test.csv");
+        let content = std::fs::read_to_string("target/test.csv").unwrap();
+        let csv = super::read_from_csv(content);
+
+        assert_eq!(csv[0][0], String::from("one"));
     }
- }
+}
+
